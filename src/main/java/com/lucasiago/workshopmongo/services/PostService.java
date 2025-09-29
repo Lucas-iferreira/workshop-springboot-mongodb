242c420e0a6811efc3351c8f6a28d13931850bda
Lucas <lucas.iferreir@gmail.com>
2025-09-29 10:57:10 -0300

    Consulta simples com query methods

--- src/main/java/com/lucasiago/workshopmongo/services/PostService.java
+++ src/main/java/com/lucasiago/workshopmongo/services/PostService.java
@@ -6,14 +6,20 @@ import com.lucasiago.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class PostService {
 
     @Autowired
     private PostRepository postRepository;
 
-    public Post findById(String id){
-        Post post = postRepository.findById(id).orElseThrow(()-> new ObjectNotFoundException(id));
+    public Post findById(String id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException(id));
         return post;
+    }
+
+    public List<Post> findByTitle(String title) {
+        return postRepository.findByTitleContainingIgnoreCase(title);
     }
 }
